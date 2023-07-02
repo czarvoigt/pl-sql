9ac44af165e23ffb5cf0f4b36dec344748c886a5
Cesar <czarvoigt@gmail.com>
2023-07-02 01:06:02 -0300

    Primeiro

---
+++ function.sql
@@ -0,0 +1,18 @@
+create or replace function F_RETURN_SAL(id in emp.empno%type)
+  return integer is
+  FunctionResult integer;
+  vsal           integer;
+  vcad           integer;
+begin
+  select nvl(count(*), 0) into vcad from emp e where e.empno = id;
+
+  if vcad <> 0 then
+  
+    select sal into vsal from emp where empno = id;
+    FunctionResult := vsal;
+  else
+    FunctionResult := -1;
+  end if;
+
+  return(FunctionResult);
+end F_RETURN_SAL;
