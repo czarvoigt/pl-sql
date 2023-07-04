e5b51fa6500c289d6622495be051d119d3af831a
Cesar <czarvoigt@gmail.com>
2023-07-04 00:25:46 -0300

    Fibo

---
+++ Fibonacci.sql
@@ -0,0 +1,15 @@
+
+DECLARE
+    VAR1   NUMBER :=0;
+    VAR2   NUMBER :=1;
+    VAR3   NUMBER :=0;
+    VCOUNT NUMBER :=0;
+BEGIN
+    WHILE (VCOUNT <= 100) LOOP
+        DBMS_OUTPUT.PUT_LINE(VAR1);
+        VAR3:=(VAR1 + VAR2);
+        VAR1:=VAR2;
+        VAR2:=VAR3;
+        VCOUNT:= VCOUNT+1;
+    END LOOP;
+END;
