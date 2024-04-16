6ae68aaf0c4da1acefc046523e2f9e0a380728a6
César Voigt <czarvoigt@gmail.com>
2024-04-15 23:12:40 -0300

    Att.

--- Fibonacci.sql
+++ Fibonacci.sql
@@ -1,11 +1,11 @@
-
+/* Imprime sequencia Fibonacci, até 100*/
 DECLARE
     VAR1   NUMBER :=0;
     VAR2   NUMBER :=1;
     VAR3   NUMBER :=0;
     VCOUNT NUMBER :=0;
 BEGIN
-    WHILE (VCOUNT <= 100) LOOP
+    WHILE (VCOUNT < 100) LOOP
         DBMS_OUTPUT.PUT_LINE(VAR1);
         VAR3:=(VAR1 + VAR2);
         VAR1:=VAR2;
